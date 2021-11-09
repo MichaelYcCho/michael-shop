d8bf2300bfbdfc5ccb6aa0c0b9cdebd4af061e52
Youngchan <michael_cho77@naver.com>
2021-11-09 19:44:00 +0900

    feature/Handling Image URL

--- frontend/src/components/product/ProductGridListSingle.js
+++ frontend/src/components/product/ProductGridListSingle.js
@@ -5,6 +5,7 @@ import { useToasts } from "react-toast-notifications";
 import { getDiscountPrice } from "../../helpers/product";
 import Rating from "./sub-components/ProductRating";
 import ProductModal from "./ProductModal";
+import { REACT_BACKEND_URL } from "utils/url";
 
 const ProductGridListSingle = ({
   product,
@@ -41,19 +42,19 @@ const ProductGridListSingle = ({
             <Link to={process.env.PUBLIC_URL + "/men/clothes/" + product.pk}>
               <img
                 className="default-img"
-                src={ `http://127.0.0.1:8000${product.product_image1}`}
+                src={ REACT_BACKEND_URL + product.product_image1}
                 alt=""
               />
               { product.product_image2 ? (
                 <img
                   className="hover-img"
-                  src={  `http://127.0.0.1:8000${product.product_image2}`}
+                  src={ REACT_BACKEND_URL + product.product_image2}
                   alt=""
                 />
               ) : (
                 <img
                 className="hover-img"
-                src={  `http://127.0.0.1:8000${product.product_image1}`}
+                src={ REACT_BACKEND_URL + product.product_image1}
                 alt=""
               />
               )}
@@ -167,19 +168,19 @@ const ProductGridListSingle = ({
                 <Link to={process.env.PUBLIC_URL + "/men/clothes/" + product.pk}>
                   <img
                     className="default-img"
-                    src={ `http://127.0.0.1:8000${product.product_image1}`}
+                    src={ REACT_BACKEND_URL + product.product_image1}
                     alt=""
                   />
                   { product.product_image2 ? (
                     <img
                       className="hover-img"
-                      src={  `http://127.0.0.1:8000${product.product_image2}`}
+                      src={ REACT_BACKEND_URL + product.product_image2}
                       alt=""
                     />
                   ) : (
                     <img
                     className="hover-img"
-                    src={  `http://127.0.0.1:8000${product.product_image1}`}
+                    src={ REACT_BACKEND_URL + product.product_image1}
                     alt=""
                   />
                   )}
--- frontend/src/components/product/ProductMainContent.js
+++ frontend/src/components/product/ProductMainContent.js
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import { useToasts } from "react-toast-notifications";
+import { REACT_BACKEND_URL } from "utils/url";
 import { getDiscountPrice } from "../../helpers/product";
 import ProductModal from "./ProductModal";
 
@@ -45,19 +46,19 @@ const ProductMainContent = ({
             <Link to={process.env.PUBLIC_URL + "/product/" + product.id}>
               <img
                 className="default-img"
-                src={ `http://127.0.0.1:8000${product.product_image1}`}
+                src={ REACT_BACKEND_URL + product.product_image1}
                 alt=""
               />
               { product.product_image2 ? (
                 <img
                   className="hover-img"
-                  src={  `http://127.0.0.1:8000${product.product_image2}`}
+                  src={ REACT_BACKEND_URL + product.product_image2}
                   alt=""
                 />
               ) : (
                 <img
                 className="hover-img"
-                src={  `http://127.0.0.1:8000${product.product_image1}`}
+                src={ REACT_BACKEND_URL + product.product_image1}
                 alt=""
               />
               )}
